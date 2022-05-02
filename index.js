24002326154326b26fda9541209e6f7c28df43ab
Susan Minott <susan_minott@yahoo.com>
2022-05-02 11:28:58 -0400

    commit tree 6aef1ea43b595307becb7349b1875e36eb59be17

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // code your solution here
+
+
+
+//1st Code
+// TEST 1
+// const sadReality = function noWins(element){
+//     return (element = 'N/A')
+// }
+
+// function superbowlWin(){
+    
+//     if (record.find === 'W'){
+//         return record.year
+//     } else{
+//         return undefined
+//     } 
+// };
+
+// TEST 3
+function superbowlWin(arr){
+    const yearBroncosWon = arr.find((arr) => arr.result === 'W');
+    if (yearBroncosWon){
+        return arr.year
+    } else{
+        return undefined
+    }
+};
+superbowlWin(record)
+
+
+// function superbowlWin(record){
+//     if (record.find === 'W'){
+//         return record.year
+//     } else{
+//         return undefined
+//     } 
+// };
+
+
+
